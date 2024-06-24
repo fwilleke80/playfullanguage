8a163353ae0ac23e1c9d747483dd343229ecfa7c
Frank Willeke <frank@frankwilleke.de>
2024-06-24 02:41:31 +0200

    Added function comments

--- lib/langlib.py
+++ lib/langlib.py
@@ -1,21 +1,35 @@
+########
+#
+# Some commonly used functions
+#
+########
+
 import re
 import string
 
+# Tokenizes a sentence, and returns the results as a list.
+# The list contains words and punctuation as seperate strings.
 def split_sentence(sentence: str) -> list:
     # Regular expression to match words and separate punctuation
     pattern = r"\w+|[^\w\s]"
     return re.findall(pattern, sentence)
 
+# Returns true if the given word starts with a vowel.
 def check_starts_with_vowel(word: str) -> bool:
     vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
     return word[0] in vowels if word else False
 
+# Returns true if the given word starts with an upper case letter.
 def check_is_first_char_uppercase(word: str) -> bool:
     return word[0].isupper() if word else False
 
+# Returns true if the given string contains only punctuation.
 def check_is_only_punctuation(s: str) -> bool:
     return all(char in string.punctuation for char in s)
 
+# Joins a list of tokens (retrieved from split_sentence(), and
+# possibly translated after that) back into a string, respecting
+# the punctuation.
 def join_sentence(tokens: list[str]) -> str:
     result = ""
     for token in tokens:
